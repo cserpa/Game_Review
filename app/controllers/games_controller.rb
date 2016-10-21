e168e8ae555c605061ff5f437f2ff913cd502248
cserpa <cserpa123@gmail.com>
2016-10-21 15:37:38 -0400

    edit and delete functionality for games, back button

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,11 @@
 class GamesController < ApplicationController
+  before_action :find_game, only: [:show, :edit, :update, :destroy]
 
   def index
+    @games = Game.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -16,14 +21,36 @@ class GamesController < ApplicationController
     else
       flash[:notice] = @game.errors.full_messages.join(', ')
       render 'new'
-
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @game.update(game_params)
+      flash[:notice] = 'Game updated successfully!'
+      redirect_to game_path(@game)
+    else
+      flash[:notice] = @game.errors.full_messages.join(', ')
+      render 'edit'
+    end
+  end
+
+  def destroy
+    flash[:notice] = 'Game deleted successfully'
+    @game.destroy
+    redirect_to root_path
   end
 
   private
 
     def game_params
       params.require(:game).permit(:title, :developer, :description)
+    end
+
+    def find_game
+      @game = Game.find(params[:id])
     end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,5 @@
 class Game < ApplicationRecord
-
   validates :title, presence: true
   validates :developer, presence: true
-  validates :description, presence: true, length: { maximum: 200 }
+  validates :description, presence: true, length: { maximum: 350 }
 end
