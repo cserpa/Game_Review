0aff95b5b4eaa775177f8716d19e0fce1c86bd81
cserpa <cserpa123@gmail.com>
2016-10-21 15:12:24 -0400

    added simple_form and bootstrap gems, new_game page and form for creating new game, plus validations

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,29 @@
 class GamesController < ApplicationController
+
   def index
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      flash[:notice] = 'Game added successfully!'
+      redirect_to root_path
+    else
+      flash[:notice] = @game.errors.full_messages.join(', ')
+      render 'new'
+
+    end
+  end
+
+  private
+
+    def game_params
+      params.require(:game).permit(:title, :developer, :description)
+    end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+
+  validates :title, presence: true
+  validates :developer, presence: true
+  validates :description, presence: true, length: { maximum: 200 }
 end
