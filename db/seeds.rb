e87d5c8744ba8ce249da6a7474f27e798afddf82
cserpa <cserpa123@gmail.com>
2016-10-25 11:22:27 -0400

    changed name to VGReviews, added seed data for platforms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Platform.create(name: "Playstation 4")
+Platform.create(name: "Xbox One")
+Platform.create(name: "PC")
+Platform.create(name: "WiiU")
+Platform.create(name: "Playstation 3")
+Platform.create(name: "Xbox 360")
+Platform.create(name: "Wii")
+Platform.create(name: "3DS")
+Platform.create(name: "Playstation Vita")
